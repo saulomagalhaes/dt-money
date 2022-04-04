5bc2053e25cbd8db1ffb668d33a241008acfe659
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-04-04 11:53:43 -0300

    feat: Configurando MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; // Captar todas chamadas com /api
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de WebSite',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
